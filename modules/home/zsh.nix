26adf1c45e9ce8cbbda39ba80817791ca4c73b74
Xavier de Place <xavier.de-place@epita.fr>
2024-09-30 09:27:37 +0200

    fix(codestats): Code::Stats now working

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -5,8 +5,16 @@
     enable = true;
     autocd = true;
     shellAliases = import ./zsh-aliases.nix;
+    initExtra = ''
+      cd() {
+        builtin cd "$@" && lsd
+      }
+      source $HOME/.config/dotfiles/plugins/codestats.plugin.zsh
+    '';
     enableCompletion = true;
-    localVariables = { };
+    localVariables = {
+      CODESTATS_API_KEY = "SFMyNTY.V0dGMmFXVnlNbkE9IyNNak0zTVRJPQ.a5GhCOWzL9pePvh7CUGF79nGGvd5FuBgvnneSpwECTQ";
+    };
     syntaxHighlighting.enable = true;
     autosuggestion.enable = false;
   };
