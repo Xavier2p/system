d1c2ca93e8c58cfcde47ab7cc3c331ce8bf9cdc5
Xavier de Place <xavier.de-place@epita.fr>
2024-11-14 21:19:06 +0100

    fix(mandalore): network manager off

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,11 +1,11 @@
-{
+{lib, ...}: {
   networking = {
     # Domain for all hosts.
     domain = "forge";
     # Disable IPv6.
     enableIPv6 = false;
     # Enable NetworkManager.
-    networkmanager.enable = true;
+    networkmanager.enable = lib.mkDefault true;
     # Enable the firewall.
     firewall.enable = true;
   };
